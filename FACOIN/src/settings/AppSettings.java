42bc4f8873473a98a2b25c2fd58858a0a475443b
Furkan ŞAHİN <31508678+furkansahinfs@users.noreply.github.com>
2021-06-07 10:42:45 +0300

    Hide the CryptoCompareAPI Key

--- FACOIN/src/settings/AppSettings.java
+++ FACOIN/src/settings/AppSettings.java
@@ -7,7 +7,7 @@ import javax.swing.ImageIcon;
  */
 public class AppSettings {
 
-	public final static String CRYPTO_API_KEY = "dcced3e12da91813d0ee3d251e38abbb8433aa93f8dcc8d4743d38da0140f142";
+	public final static String CRYPTO_API_KEY = "HIDDEN";
 	public final static String DATE_ENDPOINT = "https://showcase.api.linx.twenty57.net/UnixTime/tounixtimestamp?datetime=now";
 	public final static String CANDLE_PERIOD = "30";
 	public static String DAY_CANDLE_ENDPOINT = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=%s&tsym=%s&limit="
